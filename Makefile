302aadedd6bc798b5e85613248bee762150ea8b0
Simson Garfinkel <simsong@acm.org>
2013-05-17 00:45:47 -0400

    added

--- Makefile
+++ Makefile
@@ -1,5 +1,20 @@
-textbook.pdf: textbook.tex
+textbook.pdf: textbook.tex textbook.bib Makefile
 	pdflatex textbook
-	bibtex textbook
+	echo makeindex textbook -s StyleInd.ist
+	biber textbook
 	pdflatex textbook
 	pdflatex textbook
+
+plainbook: plainbook.pdf
+
+pb: plainbook.pdf
+
+plainbook.pdf: plainbook.tex 
+	pdflatex plainbook
+	bibtex plainbook
+	pdflatex plainbook
+	pdflatex plainbook
+
+clean:
+	/bin/rm -f *.{aux,bbl,bcf,blg,log,idx,ptc,run.xml,toc}
+	/bin/rm -f main.pdf plainbook.pdf textbook.pdf
--- fs/mbrdecode.py
+++ fs/mbrdecode.py
@@ -1,3 +1,10 @@
+# mbrdecode.py:
+# This program opens a disk image in binary mode,
+# reads the first sector, verifies the MBR signature bytes,
+# and then prints the value of the four slots in the standard
+# partition table. Notice the use of struct.unpack() to unpack
+# the little endian Byte and Long values
+
 if __name__=="__main__":
     import argparse,sys,struct
     parser = argparse.ArgumentParser(description="Print the MBR")
@@ -11,7 +18,7 @@ if __name__=="__main__":
             raise RuntimeError(img.name + " is not an MBR image")
         for i in range(0,4):
             loc = 446+i*16
-            ptype = struct.unpack('<B',mbr[loc+4:loc+5])[0]
-            lba_s = struct.unpack('<L',mbr[loc+8:loc+12])[0]
+            ptype = struct.unpack('<B',mbr[loc+4:loc+5])[0] 
+            lba_s = struct.unpack('<L',mbr[loc+8:loc+12])[0] 
             print("Partition {} offset: {:X}h type: {:3} LBA Start: {:8}".format(
                     i+1,loc,ptype,lba_s))
