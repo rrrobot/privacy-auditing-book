86bf0dc96ac072fcacd810c767daa2da469c85fd
Simson Garfinkel <simsong@acm.org>
2013-05-18 23:01:27 -0400

    added more file system stuff

--- fs/clear.py
+++ fs/clear.py
@@ -1,14 +1,15 @@
 if __name__=="__main__":
-    import argparse,sys
+    import argparse
     parser = argparse.ArgumentParser(description="Clear a raw disk partition")
-    parser.add_argument("-b","--blocksize",help="Specify block size",type=int,default=512)
+    parser.add_argument("-b","--blocksize",type=int,default=512,
+                        help="Specify block size")
     parser.add_argument("device",help="Device",type=str)
     args = parser.parse_args()
 
     count = 0
-    zbuf  = '\000' * args.blocksize
     with open(args.device,"wb") as f:
         while not f.eof():
-            f.write(zbuf)
+            f.write('\000' * args.blocksize)
             count += 1
-    print("{:,} sectors ({:,} MB) were cleared".format(count,count*args.blocksize/1e6))
+    print("{:,} sectors ({:,} MB) were cleared"
+          .format(count,count*args.blocksize/1e6))
